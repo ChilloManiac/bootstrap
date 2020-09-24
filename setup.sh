c3c1f4de5eebed7c8365569d9c4604204438bec9
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-09-24 11:59:23 +0200

    Changed ordering

--- setup.sh
+++ setup.sh
@@ -8,16 +8,17 @@ rm -rf yay
 # Install packages
 yay -S - --noconfirm < ./pkglist.txt
 
+# Set shell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+
 # Get dotfiles and stow
+rm ~/.zshrc
 mkdir /home/cnor/.config
 git clone https://github.com/ChilloManiac/dotfiles.git /home/cnor/stow
 cd /home/cnor/stow
 stow confs
 cd ~
-
-# Set shell
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
 # Setup Greeter
 
@@ -28,4 +29,5 @@ mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
 # TODO
 ## Wayland Environments (firefox etc)
+## Add hotkey to grim/slurp
 
