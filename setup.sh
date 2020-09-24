24968396d51d2b833e011f92ae52c0ccc06c8d33
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-09-24 12:07:56 +0200

    Stopped launching zsh after install

--- setup.sh
+++ setup.sh
@@ -8,10 +8,6 @@ rm -rf yay
 # Install packages
 yay -S - --noconfirm < ./pkglist.txt
 
-# Set shell
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
-
 # Get dotfiles and stow
 rm ~/.zshrc
 mkdir /home/cnor/.config
@@ -19,6 +15,11 @@ git clone https://github.com/ChilloManiac/dotfiles.git /home/cnor/stow
 cd /home/cnor/stow
 stow confs
 cd ~
+
+# Set shell
+chsh -s /usr/bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
 # Setup Greeter
 
