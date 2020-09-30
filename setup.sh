799176b581e105ccaa63b2703bd954eb80bf0c71
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-09-30 18:00:48 +0200

    somethings lol

--- setup.sh
+++ setup.sh
@@ -41,9 +41,6 @@ sudo systemctl enable sddm.service
 
 # Config Vim?
 npm g -i neovim
-pip3 install pynvim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 #reboot
 
