6a9382da9c93bd278254fdfbd3e707981122b32e
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-10-03 18:08:49 +0200

    FASD

--- setup.sh
+++ setup.sh
@@ -43,6 +43,10 @@ npm g -i neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+
+# Setup sddm
+sudo systemctl enable sddm.service
+
 #reboot
 
 
@@ -57,3 +61,4 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 ## get fasd
 ## Completions for kitty
 ## Probably use lightdm-webkit-greeter instead of sddm? https://github.com/iamsubhranil/Rice/blob/master/lightdm-webkit2-greeter_executive.md
+# https://github.com/off-world/lightdm-tiny-greeter
