17f97691e545464b6ca4f668619666395ada9686
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-10-07 20:46:56 +0200

    DONE?

--- setup.sh
+++ setup.sh
@@ -55,11 +55,9 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 
-#reboot
+reboot
 
 
 # TODO
 ## Get Sway-borders when they are on AUR?
-## Nord theme all the things, maybe including swaylock
-## Get real background
 
